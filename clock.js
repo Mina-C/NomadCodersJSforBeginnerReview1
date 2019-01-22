5d0e340b85470dda4abce73b3f42bd6ac3e1fded
meanyMina <42491537+meanyMina@users.noreply.github.com>
2019-01-22 22:48:24 +0100

    ver1. clock function
    * HTML에 CSS와 JS 파일을 링크하는 것!
        * CSS는 link tag을 사용하고, head에 넣음
        * JS는 script tag를 사용하고, body에 넣음, 여러개 나열 가능
    * function이든, 변수든 작명도 참 중요하구나...
    * 브라우저에서 변수 선언을하고, 이리 저리 사용해보는 것도 가능하구나
        * ex) const date = new Date(); 입력 후, date.getDate() 이런식으로 getDate 외에 어떤 값으로 조회할 수 있는지 확인 가능 하다, 그리고 어떤 식으로 값이 보여지는지 확인 가능
    * HTML에서 <div>에 class를 추가, 필요한 정보를 넣을 h tag를 추가 
    => document.querySelector(".class") 변수에 h tag도 querySelector로 선택
    => 필요한 값을 innerText로 변경해준다.
    => 변수.innerText = `${XXXX}` 식으로 back tik을 사용하여 정보를 표시하면 편하다. + {} 안에서 if도 사용 가능하다!!
    * setInterval(fn,sec)을 넣는데, function을 넣을 때, XXX()로 넣으면 안됨! 꼭 괄호를 빼고 function 이름만 넣어야함
        *  괄호없이 쓰는건 function을 호출하는 것인데, 필요할 때(마다) 호출하는 것
    괄호를 같이 쓰면 지금 바로 호출하는 것임

---
+++ clock.js
@@ -0,0 +1,29 @@
+const dateContainer = document.querySelector(".js-date"),
+    timeContainer = document.querySelector(".js-time"),
+    dateTitle = dateContainer.querySelector("h1"),
+    timeTitle = timeContainer.querySelector("h1");
+
+function getDate(){
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth() +1;
+    const dateOfMonth = date.getDate();
+    const day = date.getDay();
+    const dayArr = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'];
+    const dayOfWeek = dayArr[`${day}`-1];
+    dateTitle.innerText = `${year}-${month<10? `0${month}`:`${month}`}-${dateOfMonth<10? `0${dateOfMonth}`: `${dateOfMonth}`} ${dayOfWeek}`
+}
+
+function getTime(){
+    const date = new Date();
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+    const second = date.getSeconds();
+    timeTitle.innerText = `${hour}:${minute<10? `0${minute}`:`${minute}`}:${second<10? `0${second}`:`${second}`}`;
+}
+
+function init(){
+    setInterval(getDate,1000);
+    setInterval(getTime,1000);
+}
+init();
